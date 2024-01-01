caea1007d40b4b8cb55ad841db480157c1fee776
Senyo Fiawornu <contactsenyo@gmail.com>
2024-01-01 18:29:29 +0000

    passed lint test

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-'''Pagination sample.
+'''Hypermedia pagination sample.
 '''
 import csv
-from typing import List, Tuple
+import math
+from typing import Dict, List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -37,7 +38,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        '''Returns a page of data.
+        '''Cached dataset
         '''
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
@@ -46,3 +47,19 @@ class Server:
         if start > len(data):
             return []
         return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        '''Returns a page of data.
+        '''
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
+        }
+        return page_info
